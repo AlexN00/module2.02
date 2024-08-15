0cd032697c9df859b431e1739549bd7d21e03b21
AlexN00 <ya.ratatouille7@gmail.com>
2024-08-15 17:33:42 +0300

    Update Module02.02.py

--- Module02.02.py
+++ Module02.02.py
@@ -3,7 +3,7 @@ second = input()
 third = input()
 if fist == second and fist == third:
     print(3)
-if fist==second or fist==third:
+elif fist==second or fist==third or second==third:
     print(2)
 else:
     print(0)
